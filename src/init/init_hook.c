a939de42f39fe816698e6d0ad7c4aeb6e2bb014e
Romain Jacq <rjacq@z3r12p5.42lyon.fr>
2024-05-21 14:52:52 +0200

    del unused

--- src/init/init_hook.c
+++
@@ -1,9 +0,0 @@
-#include "mlx.h"
-#include "hook.h"
-
-void	init_hook(t_data *data)
-{
-	mlx_hook(data->win, 2, 1L << 0, key_press_hook, data);
-	mlx_hook(data->win, 6, 1L << 6, mouse_move_hook, data);
-	mlx_loop_hook(data->mlx, no_event, data);
-}
